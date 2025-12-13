4f0a19295e9990de6a974c0c95194f53178257fb
irvan photon <irvan.septian@photon.com>
2025-12-13 14:42:23 +0700

    [MODIFY][Component] Button - moved interface as 1 file

--- components/atoms/Button/index.tsx
+++ components/atoms/Button/index.tsx
@@ -1,16 +1,7 @@
 import React from "react";
+import { ButtonProps } from "./interface";
+
 import "./styles.scss";
-
-export type ButtonVariant = "primary" | "secondary" | "text";
-export type ButtonSize = "small" | "medium" | "large";
-
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: ButtonVariant;
-  size?: ButtonSize;
-  fullWidth?: boolean;
-  children: React.ReactNode;
-}
 
 export const Button: React.FC<ButtonProps> = ({
   variant = "primary",
