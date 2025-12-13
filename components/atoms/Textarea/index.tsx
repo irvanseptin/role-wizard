14fc2f61fafd0a130d85a9319a6dddd18c55c696
irvan photon <irvan.septian@photon.com>
2025-12-13 16:07:40 +0700

    [ADD][Component] Textarea as atom

---
+++ components/atoms/Textarea/index.tsx
@@ -0,0 +1,17 @@
+import React, { forwardRef } from "react";
+import { TextAreaProps } from "./interface";
+import "./styles.scss";
+
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ error = false, className = "", ...props }, ref) => {
+    const classes = ["textarea", error && "textarea--error", className]
+      .filter(Boolean)
+      .join(" ");
+
+    return <textarea ref={ref} className={classes} {...props} />;
+  }
+);
+
+TextArea.displayName = "TextArea";
+
+export default TextArea;
