41530308d45a0dfb146c9b0309c2747895cc1614
irvan photon <irvan.septian@photon.com>
2025-12-13 16:47:07 +0700

    [ADD][Component] Input as atom

---
+++ components/atoms/Input/index.tsx
@@ -0,0 +1,31 @@
+import React, { forwardRef } from "react";
+import { InputProps } from "./interface";
+import "./styles.scss";
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error = false, leftIcon, className = "", ...props }, ref) => {
+    const classes = [
+      "input",
+      error && "input--error",
+      leftIcon && "input--with-icon",
+      className,
+    ]
+      .filter(Boolean)
+      .join(" ");
+
+    if (leftIcon) {
+      return (
+        <div className="input-wrapper">
+          <span className="input-icon">{leftIcon}</span>
+          <input ref={ref} className={classes} {...props} />
+        </div>
+      );
+    }
+
+    return <input ref={ref} className={classes} {...props} />;
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
