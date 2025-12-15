5414f12d68b97f4a5bcba773498c8087b341805b
irvan photon <irvan.septian@photon.com>
2025-12-15 22:05:04 +0700

    Fix case-sensitive filesystem

---
+++ components/molecules/PhotoPreview/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Icon from "@/components/atoms/Icon";
+import { PhotoPreviewProps } from "./interface";
+import "./styles.scss";
+
+export const PhotoPreview: React.FC<PhotoPreviewProps> = ({
+  src,
+  alt,
+  onRemove,
+}) => {
+  return (
+    <div className="photo-preview">
+      <img src={src} alt={alt} className="photo-preview__image" />
+      <button
+        type="button"
+        onClick={onRemove}
+        className="photo-preview__remove"
+        aria-label="Remove photo"
+      >
+        <Icon name="X" size={16} />
+      </button>
+    </div>
+  );
+};
+
+export default PhotoPreview;
---
+++ components/molecules/PhotoPreview/interface.ts
@@ -0,0 +1,5 @@
+export interface PhotoPreviewProps {
+  src: string;
+  alt: string;
+  onRemove: () => void;
+}
